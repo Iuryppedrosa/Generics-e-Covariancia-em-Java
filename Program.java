69fd56e8d52e3834ce342180211c59f132439565
iurypedrosa <iuryppedrosa@protonmail.com>
2023-10-10 21:11:36 -0300

    commit unico

---
+++ Program.java
@@ -0,0 +1,32 @@
+package problema4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+        List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+        List<Double> myDouble = Arrays.asList(3.14, 6.28);
+        List<Object> myObjcts = new ArrayList<>();
+
+
+        copy(myInts, myObjcts);
+        printList(myObjcts);
+        copy(myDouble, myObjcts);
+        printList(myObjcts);
+    }
+
+    public static void copy(List<? extends Number> source, List<? super Number> destiny){
+        for(Number number : source){
+            destiny.add(number);
+        }
+    }
+
+    public static void printList(List<?> list){
+        for(Object obj : list){
+            System.out.print(obj + " ");
+        }
+        System.out.println();
+    }
+}
